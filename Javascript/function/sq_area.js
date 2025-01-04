38f274db541312268265c1374002afb1d9d43d92
varshavikraman <varshavikraman97@gmail.com>
2025-01-04 19:02:54 +0530

    update

---
+++ Javascript/function/sq_area.js
@@ -0,0 +1,8 @@
+
+       
+        function handleclick(event){
+          const side = document.getElementById("side").value
+          const area=side*side;
+          document.getElementById("demo").innerHTML=area;
+        }
+
